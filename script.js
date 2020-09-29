0ff651f17a0f69d2e436bd806f42ca96623869ca
Umberto Di Napoli <khelthos@hotmail.it>
2020-09-29 10:57:38 +0200

    All functionalities implemented

---
+++ script.js
@@ -0,0 +1,73 @@
+const imgContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+// Unsplash API
+const count = 30;
+const apiKey = "csCS8dKME4Et4puIzmQPaqkxrKoK_l86lBWW1oA_7Zk";
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+
+function imageLoaded() {
+  imagesLoaded++;
+  if (imagesLoaded === totalImages) {
+    ready = true;
+    loader.hidden = true;
+  }
+}
+
+// Helper function
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attributes[key]);
+  }
+}
+
+function displayPhotos() {
+  totalImages = photosArray.length;
+  imagesLoaded = 0;
+  //
+  photosArray.forEach((photo) => {
+    // create <a> to link unsplash page
+    const item = document.createElement("a");
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    img.addEventListener("load", imageLoaded);
+    item.appendChild(img);
+    imgContainer.appendChild(item);
+  });
+}
+
+// Get photos from unsplash API
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    photosArray = await response.json();
+    displayPhotos();
+  } catch (error) {}
+}
+
+//
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    ready = false;
+
+    getPhotos();
+  }
+});
+
+getPhotos();
