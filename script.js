62394f97882dba675fe2f27aa2d1c4e53d6384ca
Khelthos <khelthos@hotmail.it>
2020-09-29 19:00:10 +0200

    Added sweetalert2 and back on top button

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const imgContainer = document.getElementById("image-container");
 const loader = document.getElementById("loader");
+const topBtn = document.getElementById("topBtn");
 let ready = false;
 let imagesLoaded = 0;
 let totalImages = 0;
 let photosArray = [];
+let isInitialLoad = true;
+let checkResponse = "";
 
 // Unsplash API
-const count = 30;
+let photoCount = 5;
 const apiKey = "csCS8dKME4Et4puIzmQPaqkxrKoK_l86lBWW1oA_7Zk";
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
+let apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${photoCount}`;
+
+function updateApiUrl(newCount) {
+  apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${newCount}`;
+}
 
 function imageLoaded() {
   imagesLoaded++;
@@ -53,21 +60,45 @@ function displayPhotos() {
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
+    checkResponse = response.status;
     photosArray = await response.json();
     displayPhotos();
-  } catch (error) {}
+    if (isInitialLoad) {
+      updateApiUrl(30);
+      isInitialLoad = false;
+    }
+  } catch (error) {
+    let errText = error;
+    if (checkResponse === 403) {
+      errText = "Unsplash API hourly requests terminated";
+    }
+    swal.fire({
+      icon: "error",
+      title: "Oops...Something went wrong!",
+      text: errText,
+    });
+  }
 }
 
-//
+// Add a listener on scroll, when reach almost the end page new photos must be acquired
 window.addEventListener("scroll", () => {
   if (
     window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
     ready
   ) {
     ready = false;
-
     getPhotos();
   }
+
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+});
+
+topBtn.addEventListener("click", () => {
+  window.scrollTo(0, 0);
 });
 
 getPhotos();
